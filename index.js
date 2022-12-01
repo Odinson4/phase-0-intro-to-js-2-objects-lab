2d49ba38c6069603de7786bbf10f6070b10f5620
Dylan Abbott <dylanabbott33@yahoo.com>
2022-11-30 15:10:23 -0900

    commit tree 035c8be97b1192b0442fd044865b9f0af2aa9438

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
-// Write your solution in this file!
+const employee = { 
+    name: 'Dylan',
+    streetAddress: '1368 Wolff Street',
+};
+
+    function updateEmployeeWithKeyAndValue(employee, key, value) {
+    
+        const newObj = {  
+            name: "Sam",
+            streetAddress: "11 Broadway" 
+    };
+        newObj[key] = value;
+        return newObj;
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+
+    employee[key] = value;
+    return employee;
+
+}
+
+let obj = {
+    name: "Sam",
+    streetAddress: "11 Broadway", 
+    };
+
+    destructivelyUpdateEmployeeWithKeyAndValue(obj, "streetAddress", "12 Broadway");
+
+        deepEqual(obj, { name: "Sam", streetAddress: "12 Broadway" } );
+
+    
+function deleteFromEmployeeByKey(employee, key, value) {
+    
+    const newEmployee = {  
+        name: "Sam",
+        streetAddress: "11 Broadway" 
+        };
+
+        delete newEmployee[key];
+        return newEmployee;
+        
+    }
+
+    function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+
+        employee[key] = value;
+        return employee;
+    
+    }
+    
+    let key = {
+        name: "Sam",
+        streetAddress: "11 Broadway", 
+        };
+    
+        destructivelyDeleteFromEmployeeByKey(key, "streetAddress", "12 Broadway");
+    
+            deepEqual(key, { name: "Sam", streetAddress: "12 Broadway" } );
+
+    
